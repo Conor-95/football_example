144764a2c6e028a6d2b21417c9589974f31fc0d5
conor-95 <c.barron4853@student.leedsbeckett.ac.uk>
2024-11-14 10:34:22 +0000

    added try and except

--- main.py
+++ main.py
@@ -1,15 +1,31 @@
+#! /usr/bin/python3
+
 if __name__ == '__main__':
     matches = []
     line = ""
-    f = open("matches.txt")
+    try:
+        f = open("matches.txt")
+    except FileNotFoundError:
+        print("File not found")
+        exit(-1)
     print("reading file")
     for line in f:
         split_list = line.split(" ")    # split into 4 element list
+        if len(split_list) != 4:
+            print("error within match data :", line, end="")
+            continue
+        try:
+            split_list[2] = int(split_list[2])
+            split_list[3] = int(split_list[3])
+        except ValueError:
+            print("invalid score :", line, end="")
+            continue
+
         matches.append(split_list)
     print("R E S U L T S")
     for match in matches:
         home_team = match[0].strip()
         away_team = match[1].strip()
-        home_score = match[2].strip()
-        away_score = match[3].strip()
+        home_score = match[2]
+        away_score = match[3]
         print(home_team, " ", home_score, " : ", away_score, " ", away_team)
